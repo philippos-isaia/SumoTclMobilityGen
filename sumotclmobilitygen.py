f0a825d66dbb29640e11e6e364a756d267d7c5eb
philippos-isaia <philippos.isaia@gmail.com>
2019-09-12 15:59:45 +0300

    First version

--- sumotclmobilitygen.py
+++ sumotclmobilitygen.py
@@ -15,11 +15,25 @@ if args.input and args.output:
         vehicles=item.getElementsByTagName('vehicle')
         for vehicle in vehicles:
             if int(vehicle.getAttribute('id')) not in cars:
-                f.write("$node_("+str(vehicle.getAttribute('id'))+") set X_ "+str(vehicle.getAttribute('x'))+"\n")
-                f.write("$node_("+str(vehicle.getAttribute('id'))+") set Y_ "+str(vehicle.getAttribute('y'))+"\n")
                 cars.append(int(vehicle.getAttribute('id')))
-            f.write("$ns_ at "+str(vectime)+" \"$node_("+str(vehicle.getAttribute('id'))+") setdest "+str(vehicle.getAttribute('x'))+" "+str(vehicle.getAttribute('y'))+" "+str(vehicle.getAttribute('speed'))+"\"\n")
+    cars.sort()
+    x=0
+    testdict={}
+    for car in cars:
+        testdict.update({car:x})
+        x=x+1
+    newcars=[]
+    for item in items:
+        vectime = item.getAttribute('time')
+        vehicles=item.getElementsByTagName('vehicle')
+        for vehicle in vehicles:
+            if int(vehicle.getAttribute('id')) not in cars:
+                f.write("$node_("+str(testdict[int(vehicle.getAttribute('id'))])+") set X_ "+str(vehicle.getAttribute('x'))+"\n")
+                f.write("$node_("+str(testdict[int(vehicle.getAttribute('id'))])+") set Y_ "+str(vehicle.getAttribute('y'))+"\n")
+                cars.append(int(vehicle.getAttribute('id')))
+            f.write("$ns_ at "+str(vectime)+" \"$node_("+str(testdict[int(vehicle.getAttribute('id'))])+") setdest "+str(vehicle.getAttribute('x'))+" "+str(vehicle.getAttribute('y'))+" "+str(vehicle.getAttribute('speed'))+"\"\n")
     f.close()
+    print("Number of Nodes: ",len(cars))
 else:
     if not args.input:
         print("Please define the SUMO fcd input filename")
